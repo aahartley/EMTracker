93f1aa0e98dc1dbc6be7babd4f309ab06360993e
aahartley <50050814+aahartley@users.no.reply.github.com>
2021-09-18 14:33:53 -0400

    Add project files.

---
+++ EMTracker/Data/EMTrackerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EMTracker.Models;
+
+namespace EMTracker.Data
+{
+    public class EMTrackerContext : DbContext
+    {
+        public EMTrackerContext (DbContextOptions<EMTrackerContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<EMTracker.Models.Sample> Sample { get; set; }
+    }
+}
